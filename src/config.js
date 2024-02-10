d747998217d24e0278384c9afeb0722e5c4225a2
EC2 Default User <ec2-user@ip-172-31-30-64.eu-west-2.compute.internal>
2024-02-10 20:11:24 +0000

    Configure API invokeURL

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     api: {
-        invokeUrl: 'https://2ke1intz8l.execute-api.eu-west-2.amazonaws.com/prod' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://dbunhqrjz2.execute-api.eu-west-2.amazonaws.com/prod'
     }
 }
